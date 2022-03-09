ba868ff61f78ca5dac54d4ff15ae46fba2365733
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-03-10 01:18:11 +0900

    chapter-프로젝트 환경설정

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+// Repository -> Entity를 찾아줌 (DAO랑 비슷한 역할)
+// DAO : Data Access Object
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId(); // 김영한 강사님 스타일 -> 쿼리와 커맨드를 분리
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+
+}
